e198b1b0ad7cef9c164c2eacc7de8967c8970fdd
Jalal Hosseini <jalalhosseiny@gmail.com>
2021-12-04 01:45:24 +0000

    enhance cardinality

--- src/com/github/artronics/fhir/utils.clj
+++ src/com/github/artronics/fhir/utils.clj
@@ -6,7 +6,7 @@
 (defn- eq-id? [a b] (= (:id a) (:id b)))
 
 (defn- apply-diff [differential snapshots]
-  "Find matching id in snapshots and merge it to differential to it.
+  "Find matching id in snapshots and merge the matching differential to it.
   Returns [merged , snapshots minus matched] or
   identity if no match was found"
   (let [matched-sn? (fn [sn-i] (when (eq-id? differential sn-i) sn-i))
@@ -20,7 +20,7 @@
   (let [[merged sn-x] (apply-diff df-i sn-x)]
     [(conj acc merged) sn-x]))
 
-
+;; TODO: Should we consider merging lists as well? Currently, when we merge the lists will be substituted completely.
 (defn merge-elements [sn df]
   (let [[acc sn-x] (reduce apply-diff-reducer-fn [[] sn] df)]
     ;; concat whatever is left from snapshots i.e. the one that didn't match with any id in differentials.
@@ -48,35 +48,45 @@
                (check-returns ret sn df)))
 
 (defn cardinality [element]
-  (let [[base-min base-max derived-min derived-max]
-        [(:min element) (:max element) (:min (:base element)) (:max (:base element))]]
-    (case [base-min base-max derived-min derived-max]
-      [0 "1", 0 "1"] :optional
-      [0 "1", 0 0] :not-used
-      [0 "1", "1" "1"] :required
+  "Cardinality based on {:base {:min x :max y} :min a :max b}. Whether base's max: " * " should be interpreted as array or not
+  is up to interpretation. Refer to the table here: https://build.fhir.org/profiling.html#cardinality"
+  (let [parse #(if (= String (type %))
+                 (if (= "*" %) -1 (Integer/parseInt %))
+                 %)
+        base-min (:min (:base element))
+        base-max (:max (:base element))
+        derived-min (:min element)
+        derived-max (:max element)]
+    (case (map parse [base-min base-max derived-min derived-max])
+      [0 1, 0 0] :not-used
+      [0 1, 0 1] :optional
+      [0 1, 1 1] :required
+      ;; Similar to above but the base has max: many which means it CAN BE represented as an array
+      [0 -1, 0 0] :emtpy
+      [0 -1, 0 1] :at-most-1
+      [0 -1, 0 -1] :optional-many
+      [0 -1, 1 1] :only-1
+      [0 -1, 1 -1] :at-least-1
 
-      [0 "*", 0 0] :not-used
-      [0 "*", 0 "1"] :optional
-      [0 "*", 0 "*"] :optional-many
-      [0 "*", "1" "1"] :required
-      [0 "*", "1" "*"] :at-least-1
+      [1 1, 1 1] :required
 
-      ["1" "1", "1" "1"] :required
-
-      ["1" "*", "1" "1"] :required
-      ["1" "*", "1" "*"] :at-least-1)))
+      [1 -1, 1 1] :only-1
+      [1 -1, 1 -1] :at-least-1
+      ;; TODO: This only covers 0, 1 and "*" cases. We need to cover arbitrarily numbers e.x. :only-42
+      ;; FIXME: What is the default value if we can't figure out cardinality? Currently :optional
+      :optional)))
 
 (defn add-cardinality [elem]
   (assoc elem :cardinality (cardinality elem)))
 
-(defn add-field-name [elem, name]
+(defn add-field-name [elem]
+  "It adds :field-name which is the :path minus the `id.` part. the elem must be the one with path=`id.x.y.z`. i.e.
+  exclude the first element in :element and the rest can be considered as a field."
   (let [path (:path elem)
-        field? (string/starts-with? path (str name "."))
-        field-from-path (if field? (string/replace path (re-pattern (str name ".")) "")
-                                   nil)]
-    (assoc elem :field? field? :field-name field-from-path)))
+        field-name (clojure.string/replace-first path #"\S+?\." "")]
+    (assoc elem :field-name field-name)))
 
 (defn apply-element [name]
   (comp
     add-cardinality
-    #(add-field-name % name)))
+    #(add-field-name %)))
